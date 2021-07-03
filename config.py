7f0fcae9a952ea9f1b44efc665c4f80bab9370ad
Slavakg8 <86883756+Slavakg8@users.noreply.github.com>
2021-07-03 22:35:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-	'TOKEN': 'ODYwODY5NTkyMTMzNDAyNjc0.YOBhSA.AZtSu_sv2FrJ9wTQ5XBXBx8bHQU', # Получение токена - https://prnt.sc/uofhur
+	'TOKEN': 'ODYwODY5NTkyMTMzNDAyNjc0.YOBhSA.eNKMWbLZ9yBN1_qO5zWqSIpE5kU', # Получение токена - https://prnt.sc/uofhu
 	'NAME BOT': 'Slava', # Имя вашего бота
 	'ID': '860869592133402674', # Получение ID - https://prnt.sc/uofik2
 	'PREFIX': '#2808', # Префикс
