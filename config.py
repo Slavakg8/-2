993e3b5012ff694fb7513c9c808e6a28c238f766
Slavakg8 <86883756+Slavakg8@users.noreply.github.com>
2021-07-03 21:06:41 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+settings = {
+	'TOKEN': 'ODYwODY5NTkyMTMzNDAyNjc0.YOBhSA.AZtSu_sv2FrJ9wTQ5XBXBx8bHQU', # Получение токена - https://prnt.sc/uofhur
+	'NAME BOT': 'Slava', # Имя вашего бота
+	'ID': '860869592133402674', # Получение ID - https://prnt.sc/uofik2
+	'PREFIX': '#2808', # Префикс
+	'OWNER':'Слава#7777',    # ИЗМЕНЯЙТЕ НА СВОЙ NICK:ID (Пример: Toil#5605)
+	'OWNER NAME':'Слава' # ИМЯ ВЛАДЕЛЬЦА (Пример: Toil)
+}
